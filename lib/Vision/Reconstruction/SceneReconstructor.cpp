3214013f10fa29e515eab5980511a45aeda4c40c
Atanas Gospodinov <atanas@rakursy.com>
2013-06-01 16:25:40 +0300

    Some base work for camera pose estimation. Need to add in intrinsics now.

--- lib/Vision/Reconstruction/SceneReconstructor.cpp
+++ lib/Vision/Reconstruction/SceneReconstructor.cpp
@@ -133,7 +133,180 @@ namespace Xu
 
             bool SceneReconstructor::EstimateCameraPose(const std::shared_ptr<Core::PointOfView> &pointOfView, cv::Mat &rotationMatrix, cv::Mat &translationMatrix)
             {
-                return false;
+                // TODO implement RANSAC
+
+                std::vector<cv::Point3f> pointCloud;
+                std::vector<cv::Point2f> imagePoints;
+
+                for (int i = 0; i < features->Size(); i++)
+                {
+                    std::shared_ptr<Core::Feature> feature = features->GetFeature(i);
+                    if (feature->IsTriangulated() && !feature->IsHidden())
+                    {
+                        if (feature->HasCorrespondenceInView(pointOfView))
+                        {
+                            cv::Point3d cloudPoint = feature->GetPoint3d();
+                            cv::Point2d imagePoint = feature->GetPointInView(pointOfView);
+
+                            pointCloud.push_back(cv::Point3f(static_cast<float>(cloudPoint.x), static_cast<float>(cloudPoint.y), static_cast<float>(cloudPoint.z)));
+                            imagePoints.push_back(cv::Point2f(static_cast<float>(imagePoint.x), static_cast<float>(imagePoint.y)));
+                        }
+                    }
+                }
+
+                assert(pointCloud.size() == imagePoints.size());
+                int pointCount = pointCloud.size();
+
+                if (pointCount)
+                {
+                    std::cout << "Not enough points to find pose: " << pointCloud.size() << std::endl;
+                    return false;
+                }
+
+                Eigen::MatrixXd A(2 * pointCount, 11);
+                Eigen::MatrixXd B(2 * pointCount, 1);
+
+                for (int i = 0; i < pointCount; i++)
+                {
+                    A(2 * i + 0, 0) = pointCloud.at(i).x;
+                    A(2 * i + 0, 1) = pointCloud.at(i).y;
+                    A(2 * i + 0, 2) = pointCloud.at(i).z;
+                    A(2 * i + 0, 3) = 1.0;
+
+                    A(2 * i + 0, 4) = 0;
+                    A(2 * i + 0, 5) = 0;
+                    A(2 * i + 0, 6) = 0;
+                    A(2 * i + 0, 7) = 0;
+
+                    A(2 * i + 0, 8) = imagePoints.at(i).x * pointCloud.at(i).x;
+                    A(2 * i + 0, 9) = imagePoints.at(i).y * pointCloud.at(i).y;
+                    A(2 * i + 0, 10) = imagePoints.at(i).z * pointCloud.at(i).z;
+
+                    B(2 * i + 0, 0) = -imagePoints.at(i).x;
+
+                    A(2 * i + 1, 0) = 0;
+                    A(2 * i + 1, 1) = 0;
+                    A(2 * i + 1, 2) = 0;
+                    A(2 * i + 1, 3) = 0;
+
+                    A(2 * i + 1, 4) = pointCloud.at(i).x;
+                    A(2 * i + 1, 5) = pointCloud.at(i).y;
+                    A(2 * i + 1, 6) = pointCloud.at(i).z;
+                    A(2 * i + 1, 7) = 1.0;
+
+                    A(2 * i + 1, 8) = imagePoints.at(i).x * pointCloud.at(i).x;
+                    A(2 * i + 1, 9) = imagePoints.at(i).y * pointCloud.at(i).y;
+                    A(2 * i + 1, 10) = imagePoints.at(i).z * pointCloud.at(i).z;
+
+                    B(2 * i + 1, 0) = -imagePoints.at(i).x;
+                }
+
+                Eigen::MatrixXd X = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(B);
+
+                double error = 0.0;
+
+                for (int i = 0; i < pointCount; i++)
+                {
+                    Eigen::MatrixXd point(4, 1);
+                    point << pointCloud.at(i).x,
+                            pointCloud.at(i).y,
+                            pointCloud.at(i).z,
+                            1.0;
+
+                    Eigen::MatrixXd projectedPoint = X * point;
+                    projectedPoint(0, 0) /= -projectedPoint(2, 0);
+                    projectedPoint(1, 0) /= -projectedPoint(2, 0);
+
+                    double dx = projectedPoint(0, 0) - imagePoints.at(i).x;
+                    double dy = projectedPoint(1, 0) - imagePoints.at(i).y;
+
+                    double distance = dx * dx + dy * dy;
+
+                    error += distance;
+                }
+
+                std::cout << "Mean reprojection error: " << error << std::endl;
+
+                // TODO finish
+
+                /*
+                cv::Mat cameraMatrix = pointOfView->GetCameraParameters().GetCameraMatrix();
+                cv::Mat distortionCoefficients = pointOfView->GetCameraParameters().GetDistortionCoefficients();
+                //    Mat cameraMatrix32f, distortionCoefficients32f;
+                //    cameraMatrix.convertTo(cameraMatrix32f, CV_32FC1);
+                //    distortionCoefficients.convertTo(distortionCoefficients32f, CV_32FC1);
+
+                //    cv::Mat pointCloudMat(pointCloud); pointCloudMat = pointCloudMat.t();
+                //    cv::Mat imagePointsMat(imagePoints); imagePointsMat = imagePointsMat.t();
+                //    cv::Mat rotationVector, translationVector;
+
+                //    cv::gpu::solvePnPRansac(pointCloudMat, imagePointsMat, cameraMatrix32f, distortionCoefficients32f, rotationVector, translationVector, false);
+
+                cv::Mat prevPOVRot = lastPointOfView->GetCameraParameters().GetRotationMatrix();
+                cv::Mat rotation; cv::Rodrigues(prevPOVRot, rotation);
+                cv::Mat translation = lastPointOfView->GetCameraParameters().GetTranslationMatrix();
+                //    rotationVector.convertTo(rotation, CV_64FC1);
+                //    translationVector.convertTo(translation, CV_64FC1);
+
+                cv::solvePnPRansac(pointCloud, imagePoints, cameraMatrix, distortionCoefficients, rotation, translation, true);
+//                cv::solvePnP(pointCloud, imagePoints, cameraMatrix, distortionCoefficients, rotation, translation);
+
+                std::vector<cv::Point2f> projectedPoints;
+                cv::projectPoints(pointCloud, rotation, translation, cameraMatrix, distortionCoefficients, projectedPoints);
+
+                std::vector<int> inliers;
+                std::vector<double> reprojectionErrors;
+                for (int i = 0; i < projectedPoints.size(); i++)
+                {
+                    reprojectionErrors.push_back(cv::norm(projectedPoints[i] - imagePoints[i]));
+                    if (cv::norm(projectedPoints[i] - imagePoints[i]) < 10.0)
+                    {
+                        inliers.push_back(i);
+                    }
+                }
+
+                if (inliers.size() < (double)(imagePoints.size()) /  5.0)
+                {
+                    rotationMatrix = cv::Mat();
+                    translationMatrix = cv::Mat();
+                    std::cout << "Not enough inliers: " << inliers.size() << " / " << imagePoints.size() << std::endl;
+                    return false;
+                }
+
+                cv::Rodrigues(rotation, rotationMatrix);
+
+                std::cout << "SolvePnP output:" << std::endl;
+                std::cout << rotation << std::endl;
+                std::cout << rotationMatrix << std::endl;
+                std::cout << translation << std::endl;
+
+                translationMatrix = (-1.0 * rotationMatrix.t()) * translation;
+                rotationMatrix.at<double>(2, 0) *= -1;
+                rotationMatrix.at<double>(2, 1) *= -1;
+                rotationMatrix.at<double>(2, 2) *= -1;
+                rotationMatrix *= -1;
+                if (cv::norm(translationMatrix) > 200.0)
+                {
+                    std::cout << cv::norm(translationMatrix) << std::endl;
+                    rotationMatrix = cv::Mat();
+                    translationMatrix = cv::Mat();
+                    std::cout << "Estimated camera movement too big." << std::endl;
+                    return false;
+                }
+
+                if (cv::determinant(rotationMatrix) + 1.0 < 1e-09)
+                {
+                    rotationMatrix = cv::Mat();
+                    translationMatrix = cv::Mat();
+                    std::cout << "Rotation incoherent." << std::endl;
+                    return false;
+                }
+
+
+                std::cout << "Pose estimation successful with " << inliers.size() << " points." << std::endl;
+                */
+
+                return true;
             }
 
 
